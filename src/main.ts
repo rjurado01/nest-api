a9cb07009fb177e7405e50ff2e99c3cdf18c2d76
rjurado01 <rjurado01@gmail.com>
2022-07-05 12:37:57 +0200

    Tutorial step 17

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {ValidationPipe, ValidationError, UnprocessableEntityException} from '@nestjs/common'
+import {NestFactory} from '@nestjs/core'
+import {AppModule} from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors: ValidationError[]) => {
+        return new UnprocessableEntityException(errors)
+      },
+    }),
+  )
+
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
