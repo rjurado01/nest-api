5cbed5d63a252c532b25406e56c520aed969af12
rjurado <rjurado@nosolosoftware.es>
2022-07-07 12:47:30 +0200

    Tutorial step 46 Schema Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,27 @@ import {AppService} from './app.service'
 import {CoffeesModule} from './coffees/coffees.module'
 import {Event} from './events/entities/event.entity'
 import {CoffeeRatingModule} from './coffee-rating/coffee-rating.module'
+import {ConfigModule} from '@nestjs/config'
+import * as Joi from 'joi'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        // POSTGRES_HOST: Joi.required(),
+        // POSTGRES_PORT: Joi.number().default(5432),
+        POSTGRES_USER: Joi.required(),
+      }),
+    }),
     CoffeesModule,
     CoffeeRatingModule,
     Event,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'rjurado',
-      database: 'nest_api_dev',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: +process.env.POSTGRES_PORT || 5432,
+      username: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB || 'nest_api_dev',
       autoLoadEntities: true,
       synchronize: true,
     }),
