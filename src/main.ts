38326c82c82176e6fb939b20e93c0be4a679e984
rjurado01 <rjurado01@gmail.com>
2022-07-08 11:55:49 +0200

    Tutorial step 53 Catch Exceptions with Filters

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common'
 import {NestFactory} from '@nestjs/core'
 import {AppModule} from './app.module'
+import {HttpExceptionFilter} from './common/filters/http-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -20,6 +21,8 @@ async function bootstrap() {
       },
     }),
   )
+
+  app.useGlobalFilters(new HttpExceptionFilter())
 
   await app.listen(3000)
 }
