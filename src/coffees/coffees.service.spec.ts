3dcee4239b6f5635961fde9396013d4e3ee2d5b2
rjurado01 <rjurado01@gmail.com>
2022-07-11 11:19:42 +0200

    Tutorial 68 Adding Unit Tests

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,18 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CoffeesService } from './coffees.service';
+import {Test, TestingModule} from '@nestjs/testing'
+import {getRepositoryToken} from '@nestjs/typeorm'
+import {Connection} from 'typeorm'
+import {COFFEE_BRANDS} from './coffees.constants'
+import {CoffeesService} from './coffees.service'
+import {CoffeeFlavor} from './entities/coffee-flavor.entity'
+import {Coffee} from './entities/coffee.entity'
 
 describe('CoffeesService', () => {
-  let service: CoffeesService;
+  let service: CoffeesService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
-    }).compile();
+      providers: [
+        CoffeesService,
+        {provide: Connection, useValue: {}},
+        {provide: getRepositoryToken(CoffeeFlavor), useValue: {}},
+        {provide: getRepositoryToken(Coffee), useValue: {}},
+        {provide: COFFEE_BRANDS, useValue: []},
+      ],
+    }).compile()
 
-    service = module.get<CoffeesService>(CoffeesService);
-  });
+    // service = module.get<CoffeesService>(CoffeesService)
+    service = await module.resolve(CoffeesService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+})
